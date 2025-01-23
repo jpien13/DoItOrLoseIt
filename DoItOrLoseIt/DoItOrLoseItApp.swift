6b2e2b1c7204d810bfb79a2cdc2609ac167ca18d
Jason Pien <jason_pien@brown.edu>
2025-01-23 14:02:58 -0500

    Initial Commit

---
+++ DoItOrLoseIt/DoItOrLoseItApp.swift
@@ -0,0 +1,32 @@
+//
+//  DoItOrLoseItApp.swift
+//  DoItOrLoseIt
+//
+//  Created by Jason Pien on 1/23/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DoItOrLoseItApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ DoItOrLoseItTests/DoItOrLoseItTests.swift
@@ -0,0 +1,17 @@
+//
+//  DoItOrLoseItTests.swift
+//  DoItOrLoseItTests
+//
+//  Created by Jason Pien on 1/23/25.
+//
+
+import Testing
+@testable import DoItOrLoseIt
+
+struct DoItOrLoseItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
