ab6e78894f56c1c3a78445f32a6764dde87603ac
Jason Pien <jason_pien@brown.edu>
2025-01-27 22:34:54 -0500

    organization

---
+++ DoItOrLoseIt/Views/Screens/HomeTabView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  DoItOrLoseIt
+//
+//  Created by Jason Pien on 1/23/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct HomeTabView: View {
+    var body: some View {
+        TabView {
+            BalanceView()
+                .tabItem {
+                    Image(systemName: "dollarsign.circle")
+                    Text("Balance")
+                }
+            TaskListView()
+                .tabItem {
+                    Image(systemName: "checklist")
+                    Text("Tasks")
+                }
+            MapView()
+                .tabItem {
+                    Image(systemName: "map")
+                    Text("Map")
+                }
+                .ignoresSafeArea(edges: .top)
+                .padding(.bottom)
+            
+        }
+    }
+}
+
+#Preview {
+    HomeTabView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
