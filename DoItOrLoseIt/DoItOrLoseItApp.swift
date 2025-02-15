07135e87e5e8240fe78250833b6bed066e8afc60
Jason Pien <jason_pien@brown.edu>
2025-02-15 15:33:16 -0500

    new TODO goals

--- DoItOrLoseIt/DoItOrLoseItApp.swift
+++ DoItOrLoseIt/DoItOrLoseItApp.swift
@@ -28,14 +28,10 @@ struct DoItOrLoseItApp: App {
 // TODO: Add wager subtract and balance
 // TODO: Honor my price button
 // TODO: Watch ad to recover lost wager. Lost wagers go to charity. Ad revenue goes to me.
+
+
 /*
- Users deposit money upfront (e.g., $20 into their "Gym Commitment Fund").
- Each time they successfully check in at the gym, they unlock a portion of their deposit (e.g., $5 per gym visit).
- If they fail to check in, they don’t instantly lose money—instead, they might need extra gym visits to fully unlock their deposit.
- If they fail too often, remaining funds could be:
- Donated to a fitness-related charity (keeps it ethical & App Store-friendly).
- Converted to app credits for a retry (keeps users engaged).
- Rolled over for a new challenge instead of being lost outright.
+Honor my price is probably best (if you complete you get extra rewards)
+Keep virtual plant or character for people who wanna do it for free?
+Build momentum and have friend leaderboard or accountibility group?
  */
-
-
