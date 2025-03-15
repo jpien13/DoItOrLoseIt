5655da6c46b305cc4ef35b59aeb57119dbb18767
Jason Pien <jason_pien@brown.edu>
2025-03-15 17:41:05 -0400

    skeleton for BalanceManager

---
+++ DoItOrLoseIt/Utilities/Managers/BalanceManager.swift
@@ -0,0 +1,44 @@
+//
+//  BalanceManager.swift
+//  DoItOrLoseIt
+//
+//  Created by Jason Pien on 3/15/25.
+//
+
+import Foundation
+import CoreData
+import OSLog
+import SwiftUI
+
+class BalanceManager: ObservableObject {
+    
+    func addFunds(){
+        
+    }
+    
+    func deductWager(){
+        
+    }
+    
+    func refundWager(){
+        
+    }
+    
+    private func loadBalance(){
+        
+    }
+    
+    private func createInitBalance(){
+        
+    }
+    
+    private func loadTransactions(){
+        
+    }
+    
+    private func updateBalance(){
+        
+    }
+    
+    
+}
